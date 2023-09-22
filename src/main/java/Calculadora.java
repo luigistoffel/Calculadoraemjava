34baae0ef8f6eca1eda30e38a2a0b268f36a043d
antonio g. stoffel <luigi.stoffel@duque.g12.br>
2023-09-21 21:13:59 -0300

    criação da tela da calculadora

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 08144
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setTitle("calculadora/não me responsabilizo pelos erros");
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+    }
+}
